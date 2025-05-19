943286bd741431beb6fd2724403e06da185ed0c4
Goldmensch <pm@goldmensch.dev>
2025-05-19 20:26:16 +0200

    Minor changes

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -1,7 +1,5 @@
 module fluava.lib.main {
     requires io.github.parseworks.parseworks;
-    requires java.desktop;
-    requires java.management;
     requires dev.goldmensch.cldrplurals;
     requires io.github.kaktushose.proteus;
 }
